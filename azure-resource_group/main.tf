83b8c20b0596337edc86fc443ad01f4d6e6f8330
Siva Prasad Naidu <naidu@happygears.net>
2023-03-06 17:40:18 +0530

    created azure SG and network resources using variables.tf

--- azure-resource_group/main.tf
+++ azure-resource_group/main.tf
@@ -7,15 +7,6 @@ resource "azurerm_resource_group" "demo1" {
   }
 }
 
-resource "azurerm_resource_group" "demo2" {
-  name     = var.rg2_name
-  location = var.rg2_location
-
-  tags = {
-    environment = var.rg2_tags
-  }
-}
-
 resource "azurerm_network_security_group" "rg1_sg1" {
   name                = "rg1-security-group"
   location            = azurerm_resource_group.demo1.location
@@ -42,5 +33,43 @@ resource "azurerm_virtual_network" "rg1-vn1" {
 
   tags = {
     environment = "Testing"
+  }
+}
+
+resource "azurerm_resource_group" "demo2" {
+  name     = var.rg2_name
+  location = var.rg2_location
+
+  tags = {
+    environment = var.rg2_tags
+  }
+}
+
+resource "azurerm_network_security_group" "rg2_sg1" {
+  name                = var.rg2_security_group
+  location            = azurerm_resource_group.demo2.location
+  resource_group_name = azurerm_resource_group.demo2.name
+}
+
+resource "azurerm_virtual_network" "rg2-vn1" {
+  name                = var.rg2_virtual_network_1
+  location            = azurerm_resource_group.demo2.location
+  resource_group_name = azurerm_resource_group.demo2.name
+  address_space       = var.rg2_vn1_address_space
+  dns_servers         = var.rg2_vn1_dns_servers
+
+  subnet {
+    name           = var.rg2_vn1_subnet1_name
+    address_prefix = var.rg2_vn1_subnet1_address_prefix
+  }
+
+  subnet {
+    name           = var.rg2_vn1_subnet2_name
+    address_prefix = var.rg2_vn1_subnet2_address_prefix
+    security_group = azurerm_network_security_group.rg2_sg1.id
+  }
+
+  tags = {
+    environment = var.rg2_tags
   }
 }
--- azure-resource_group/varaibles.tf
+++ azure-resource_group/varaibles.tf
@@ -1,3 +1,11 @@
+variable "subscription_id" {}
+
+variable "tenant_id" {}
+
+variable "client_id" {}
+
+variable "client_secret" {}
+
 variable "rg2_name" {
   default     = "testResourceGroup2"
   type        = string
@@ -16,10 +24,48 @@ variable "rg2_tags" {
   description = "RG tag"
 }
 
-variable "subscription_id" {}
+variable "rg2_security_group" {
+  default     = "rg2-security-group"
+  type        = string
+  description = "RG Security group name"
+}
 
-variable "tenant_id" {}
+variable "rg2_virtual_network_1" {
+  default     = "rg2-network"
+  type        = string
+  description = "RG virtual network name"
+}
 
-variable "client_id" {}
+variable "rg2_vn1_address_space" {
+  default     = ["10.0.0.0/16"]
+  type        = list
+  description = "The address space that is used the virtual network"
+}
 
-variable "client_secret" {}
+variable "rg2_vn1_dns_servers" {
+  default     = ["10.0.0.6", "10.0.0.7"]
+  type        = list
+  description = "List of IP addresses of DNS servers"
+}
+
+variable "rg2_vn1_subnet1_name" {
+  default     = "rg2-vn1-subnet1"
+  type        = string
+  description = "The name of the subnet"
+}
+
+variable "rg2_vn1_subnet1_address_prefix" {
+  default     = "10.0.3.0/24"
+  description = "The address prefix to use for the subnet"
+}
+
+variable "rg2_vn1_subnet2_name" {
+  default     = "rg2-vn1-subnet2"
+  type        = string
+  description = "The name of the subnet"
+}
+
+variable "rg2_vn1_subnet2_address_prefix" {
+  default     = "10.0.4.0/24"
+  description = "The address prefix to use for the subnet"
+}
